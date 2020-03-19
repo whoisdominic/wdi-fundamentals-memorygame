fc43b8d6b6cb7a46a8ac82cbb2e8d0dd682ca9a6
Dominic Cobb <dominicshajaa@icloud.com>
2020-03-19 14:45:44 -0700

    created game logic

--- memory-game/app.js
+++ memory-game/app.js
@@ -1,8 +1,57 @@
-console.log("Up and running!");
+const cards = ['queen','king','queen','king'];
 
-let cardOne = "King";
-let cardTwo = "King";
-let cardThree = "Queen";
-let cardFour = "Queen";
+let cardsInPlay = []
 
-console.log('Player flipped ' + cardOne + ' and ' + cardThree);
+let cardOne = cards[0];
+let cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log(`User flipped ${cardOne}!`);
+console.log(`User flipped ${cardTwo}!`);
+
+if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert('You found a match!');
+    } else {
+        alert('Sorry, Try Again');
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//OLD CODE ---------------------------------------------------------
+//----------------------------------------------------------------//
+
+// console.log("Up and running!");
+
+// let cardOne = "King";
+// let cardTwo = "King";
+// let cardThree = "Queen";
+// let cardFour = "Queen";
+
+// console.log('Player flipped ' + cardOne + ' and ' + cardThree);
+
+
