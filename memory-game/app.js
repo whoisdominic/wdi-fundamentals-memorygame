5c1e27e1b246b324e10d1b9f21216c9bdf7ec4a5
Dominic Cobb <dominicshajaa@icloud.com>
2020-03-18 18:45:38 -0700

    created game logic

---
+++ memory-game/app.js
@@ -0,0 +1,8 @@
+console.log("Up and running!");
+
+let cardOne = "King";
+let cardTwo = "King";
+let cardThree = "Queen";
+let cardFour = "Queen";
+
+console.log('Player flipped ' + cardOne + ' and ' + cardThree);
